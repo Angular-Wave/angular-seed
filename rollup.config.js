fa44d8bc74ef332ab3664b70b374c5029e0ebea0
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-07-28 23:28:29 +0300

    Add rollup html plugin

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: "index.html",
+  output: {
+    dir: "dist",
+    entryFileNames: "[name].[hash].js",
+  },
+  plugins: [
+    html({
+      minify: true,
+    }),
+    resolve(), // tells Rollup how to find date-fns in node_modules
+    commonjs(), // converts date-fns to ES modules
+    production && terser(), // minify, but only in production
+  ],
+};
