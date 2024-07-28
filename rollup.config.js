3343b7ddaf2b7530c6b08b21faad836efc4edaa6
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-07-29 00:16:48 +0300

    Add copy task

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
+import { copy } from "@web/rollup-plugin-copy";
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -17,6 +18,7 @@ export default {
     html({
       minify: true,
     }),
+    copy({ patterns: "./*.{json,txt}", exclude: "node_modules" }),
     resolve(), // tells Rollup how to find date-fns in node_modules
     commonjs(), // converts date-fns to ES modules
     production && terser(), // minify, but only in production
