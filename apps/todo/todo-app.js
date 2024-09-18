ebf62900251b90c77a42eacdd4f924907574e2fa
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-09-19 00:13:38 +0300

    Extract CSS

--- apps/todo/todo-app.js
+++ apps/todo/todo-app.js
@@ -7,4 +7,35 @@ import TodoController from "./todo-ctl";
  * Its a great way to inject custom DOM behaviour into your server-rendered views.
  */
 
-window.angular.module("todo", []).controller("TodoCtrl", TodoController);
+window.angular
+  .module("todo", [])
+  .controller("TodoCtrl", TodoController)
+  .directive("ngWatch", () => {
+    return {
+      restrict: "A",
+
+      scope: {
+        ngModel: "=",
+        ngWatch: "=",
+      },
+      link: (scope, element, attrs, ngModelCtrl) => {
+        scope.$watch("ngModel", function (newValue) {
+          if (newValue !== scope.ngWatch) {
+            scope.ngWatch = newValue;
+            // Update the checkbox checked state
+            element.checked = newValue;
+          }
+        });
+
+        // Watch the ngWatch for changes and update the model
+        scope.$watch("ngWatch", function (newValue) {
+          debugger;
+          if (newValue !== scope.ngModel) {
+            scope.ngModel = newValue;
+            // Update the ngModel value
+            ngModelCtrl.$setViewValue(newValue);
+          }
+        });
+      },
+    };
+  });
