90bbb7f881f761c6308f1c7c7fd8b41d8c2fdb9c
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-09-29 03:08:42 +0300

    Simplify

--- apps/todo/todo-ctl.js
+++ apps/todo/todo-ctl.js
@@ -1,25 +1,3 @@
 import ToDoList from "./todo-list";
 
-export default class TodoController {
-  constructor() {
-    /** @type {ToDoList} */
-    this.list = new ToDoList();
-  }
-
-  /**
-   * Create a new task
-   * @param {String} task
-   * @return {void}
-   */
-  add(task) {
-    this.list.add(task);
-  }
-
-  /**
-   * Delete all finished tasks
-   * @return {void}
-   */
-  archive() {
-    this.list.archive();
-  }
-}
+export default class TodoController extends ToDoList {}
