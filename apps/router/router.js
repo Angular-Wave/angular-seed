5d7d83ee7b29566e720c2825115e43790c65748f
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-12-05 01:45:39 +0200

    Sample router app

---
+++ apps/router/router.js
@@ -0,0 +1,30 @@
+import "@angular-wave/angular.ts";
+
+window.angular.module("router", []).config([
+  "$stateProvider",
+  "$locationProvider",
+  ($stateProvider, $locationProvider) => {
+    $locationProvider.hashPrefixValue = "";
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false,
+      rewriteLinks: false,
+    });
+    $stateProvider
+      .state({
+        name: "page1",
+        url: "/page1",
+        template: "<h3>Its the NG-Router hello world app!</h3>",
+      })
+      .state({
+        name: "page2",
+        url: "/page2",
+        templateUrl: "/apps/router/_page2.html",
+      })
+      .state({
+        name: "home",
+        url: "/apps/router/router.html",
+        templateUrl: "/apps/router/_home.html",
+      });
+  },
+]);
