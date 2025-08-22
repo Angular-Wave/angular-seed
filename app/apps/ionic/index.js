9ac80be4bae822a195574bb22280a0489049de88
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2025-08-22 06:22:28 +0300

    Optimize Ionic initialization

--- app/apps/ionic/index.js
+++ app/apps/ionic/index.js
@@ -1,7 +1,13 @@
 import { angular } from "@angular-wave/angular.ts";
 import { PhotoController } from "./photo-controller.js";
-import { defineCustomElements } from "@ionic/pwa-elements/loader";
-defineCustomElements(window);
+import { defineCustomElements as defineIonic } from "@ionic/core/loader";
+import { defineCustomElements as definePwa } from "@ionic/pwa-elements/loader";
+import { addIcons } from "ionicons";
+import { camera, images } from "ionicons/icons";
+
+addIcons({ camera, images });
+defineIonic(window);
+definePwa(window);
 
 angular
   .module("app", [])
