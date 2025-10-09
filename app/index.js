e9c91a0e83c97eb357e0fe895f76cf4cc8cec7a6
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2025-10-09 19:35:55 +0300

    ng static namespace test

--- app/index.js
+++ app/index.js
@@ -3,6 +3,10 @@ import { angular } from "@angular-wave/angular.ts";
 angular.module("version", []).controller(
   "VersionController",
   class VersionController {
+    /** @param {ng.Scope} $scope */
+    constructor($scope) {
+      this.$scope = $scope;
+    }
     version = angular.version;
   },
 );
